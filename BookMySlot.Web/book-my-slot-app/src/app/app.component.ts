64007a9aebee4ec45e3e81008cfe19163717e84b
tarun aggarwal <tarun.aggarwal4@gmail.com>
2020-12-22 15:24:24 +0530

    Test

--- BookMySlot.Web/book-my-slot-app/src/app/app.component.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'book-my-slot-app test';
+  title = 'book-my-slot-app test 1';
 }
