b0684106d475f79a6d60229a61b735087d8cd871
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-04 18:49:39 +0530

    Code Clean Up

--- BookMySlot.Web/book-my-slot-app/src/app/services/timezone.service.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/services/timezone.service.ts
@@ -5,34 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class TimezoneService {
 
-  //constructor(private timeHours: number[], private timeMinutes: number[]) {
-
-  //  for (let i = 0; i <= 23; i++) {
-  //    timeHours.push(i);
-  //  }
-
-  //  for (let i = 0; i <= 59; i++) {
-  //    timeMinutes.push(i);
-  //  }
-
-  //}
-
-
   constructor() {
 
   }
 
+  timeZones: string[] = ['EST', 'India Standard Time'];
+
   public getTimeZones(): string[] {
-    let timeZones: string[] = ['EST', 'India Standard Time'];
-    return timeZones;
+    return this.timeZones;
   }
-
-
-  //public getHours(): number[] {
-  //  return this.timeHours;
-  //}
-
-  //public getMinutes(): number[] {
-  //  return this.timeMinutes;
-  //}
+ 
 }
