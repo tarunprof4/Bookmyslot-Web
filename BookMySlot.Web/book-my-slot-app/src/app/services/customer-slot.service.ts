fedf46b68a2e9e24706c62b55275e9d5874d4b22
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-07 14:41:12 +0530

    moved error logic

--- BookMySlot.Web/book-my-slot-app/src/app/services/customer-slot.service.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/services/customer-slot.service.ts
@@ -30,27 +30,7 @@ export class CustomerSlotService {
   private handleHttpError(error: HttpErrorResponse): Observable<ResolverError> {
     let resolverError = new ResolverError();
 
-    resolverError.statusCode = error.status;
-    console.log(error);
-
-
-    if (resolverError.statusCode == HttpStatusConstants.NotFound) {
-      resolverError.errors.push("No records found");
-    }
-
-    else if (resolverError.statusCode == HttpStatusConstants.BadRequest) {
-      resolverError.errors = error.error;
-    }
-
-    else if (resolverError.statusCode == HttpStatusConstants.InternalServerError) {
-      resolverError.errors.push("Some issue with service please try later");
-    }
-
-
-    else if (resolverError.statusCode == HttpStatusConstants.GatewayTimeOut) {
-      resolverError.errors.push("Some issue with service please try later");
-    }
-
-    return throwError(resolverError);
+    return resolverError.handleHttpError(error);
   }
+
 }
