6891ac6fa9efb82c226e868cd439df94f53c78f8
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-07 14:07:34 +0530

    Home Page

--- BookMySlot.Web/book-my-slot-app/src/app/app-routing.module.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/app-routing.module.ts
@@ -5,18 +5,19 @@ import { BookedSlotsComponent } from './booked-slots/booked-slots.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileSettingsResolverService } from './profile-settings/profile-settings-resolver.service';
 import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+import { CustomerSlotResolverService } from './services/resolvers/customer-slot-resolver.service';
 import { ShareSlotComponent } from './share-slot/share-slot.component';
 import { SharedSlotsComponent } from './shared-slots/shared-slots.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, resolve: { resolvedCustomerSlots: CustomerSlotResolverService } },
   { path: 'book-slot', component: BookSlotComponent },
   { path: 'share-slot', component: ShareSlotComponent },
   { path: 'shared-slots', component: SharedSlotsComponent },
   { path: 'booked-slots', component: BookedSlotsComponent },
   { path: 'profile-settings', component: ProfileSettingsComponent, resolve: { resolvedProfileSettings: ProfileSettingsResolverService } },
   
-  { path: '', redirectTo: '/share-slot', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
