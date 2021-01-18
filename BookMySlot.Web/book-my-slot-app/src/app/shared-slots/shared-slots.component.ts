ec80173ef589ec69ea6e0eb7d3b974271fa42c70
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-18 18:10:45 +0400

    Shared Slot

--- BookMySlot.Web/book-my-slot-app/src/app/shared-slots/shared-slots.component.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/shared-slots/shared-slots.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { EmailService } from '../services/email.service';
 import { SharedSlotService } from '../services/shared-slot.service';
 import { SlotService } from '../services/slot.service';
@@ -7,6 +8,9 @@ import { CancelledSlotDetails } from '../shared/cancelled-slot-details';
 import { HttpStatusConstants } from '../shared/constants/http-status-constants';
 import { ResolverError } from '../shared/resolver-error';
 import { ShareSlot } from '../shared/shared-slot';
+import { ModalComponent } from '../shared/ui-controls/modal-component';
+import { ModalFailureComponent } from '../ui-controls/modal-failure/modal-failure.component';
+import { ModalSuccessComponent } from '../ui-controls/modal-success/modal-success.component';
 
 @Component({
   selector: 'app-shared-slots',
@@ -21,10 +25,12 @@ export class SharedSlotsComponent implements OnInit {
   customerCompletedSlots: ShareSlot[] = [];
   customerCancelledSlots: CancelledSlotDetails[] = [];
   resolverError: ResolverError = new ResolverError();
+  private bsModalRef: BsModalRef;
+  private modalComponent = new ModalComponent();
 
   sharedSlotBy: string = "26eca53c21344dea874c99cc1df9ceef";
 
-  constructor(private sharedSlotService: SharedSlotService, private emailService: EmailService, private slotService: SlotService, private route: ActivatedRoute) { }
+  constructor(private sharedSlotService: SharedSlotService, private emailService: EmailService, private slotService: SlotService, private route: ActivatedRoute, private modalService: BsModalService) { }
 
   ngOnInit(): void {
 
@@ -46,47 +52,55 @@ export class SharedSlotsComponent implements OnInit {
 
 
 
-  
+
 
 
   onResendEmail(sharedSlotModelInformation: string) {
     this.emailService.resendSlotInformation(sharedSlotModelInformation, this.sharedSlotBy)
       .subscribe(
         (data: boolean) => {
-          console.log(data);
+          this.showSuccessModal();
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.showFailureModal(err);
+        }
       );
   }
 
 
   onBookedSlotCancel(sharedSlotModelInformation: string, index: number) {
-    
+
 
     this.slotService.cancelSlot(sharedSlotModelInformation, this.sharedSlotBy)
       .subscribe(
         (data: boolean) => {
           this.customerBookedSlots.splice(index, 1);
+          this.showSuccessModal();
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.showFailureModal(err);
+        }
       );
   }
 
   onYetToBeBookedSlotCancel(sharedSlotModelInformation: string, index: number) {
-    
+
 
     this.slotService.cancelSlot(sharedSlotModelInformation, this.sharedSlotBy)
       .subscribe(
         (data: boolean) => {
           this.customerYetToBeBookedSlots.splice(index, 1);
+          this.showSuccessModal();
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.showFailureModal(err);
+        }
       );
   }
 
 
   getBookedSlots() {
-    
+
 
     this.sharedSlotService.getCustomerBookedSlots(this.sharedSlotBy)
       .subscribe(
@@ -97,13 +111,18 @@ export class SharedSlotsComponent implements OnInit {
 
           console.log(data);
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.customerBookedSlots = [];
+          if (err.statusCode != HttpStatusConstants.NotFound) {
+            this.showFailureModal(err);
+          }
+        }
       );
 
   }
 
   getYetToBeBookedSlots() {
-    
+
 
     this.sharedSlotService.getCustomerYetToBeBookedSlots(this.sharedSlotBy)
       .subscribe(
@@ -114,13 +133,18 @@ export class SharedSlotsComponent implements OnInit {
 
           console.log(data);
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.customerYetToBeBookedSlots = [];
+          if (err.statusCode != HttpStatusConstants.NotFound) {
+            this.showFailureModal(err);
+          }
+        }
       );
 
   }
 
   getCompletedSlots() {
-    
+
 
     this.sharedSlotService.getCustomerCompletedSlots(this.sharedSlotBy)
       .subscribe(
@@ -131,27 +155,48 @@ export class SharedSlotsComponent implements OnInit {
 
           console.log(data);
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.customerCompletedSlots = [];
+          if (err.statusCode != HttpStatusConstants.NotFound) {
+            this.showFailureModal(err);
+          }
+        }
       );
 
 
   }
 
   getCancelledSlots() {
-    
+
 
     this.sharedSlotService.getCustomerCancelledSlots(this.sharedSlotBy)
       .subscribe(
         (data: CancelledSlotDetails[]) => {
           this.customerCancelledSlots = data;
-
-          console.log("got getCompletedSlots " + this.customerYetToBeBookedSlots);
-
-          console.log(data);
         },
-        (err: any) => console.log(err)
+        (err: any) => {
+          this.customerCancelledSlots = [];
+          if (err.statusCode != HttpStatusConstants.NotFound) {
+            this.showFailureModal(err);
+          }
+        }
       );
 
+  }
+
+
+  private showSuccessModal() {
+    let successModalComponent = this.modalComponent.getSuccessModalComponent();
+    this.bsModalRef = this.modalService.show(ModalSuccessComponent);
+    this.bsModalRef.content.title = successModalComponent.title;
+    this.bsModalRef.content.bodyItems = successModalComponent.bodyItems;
+  }
+
+  private showFailureModal(err: any) {
+    let failureModalComponent = this.modalComponent.getFailureModalComponent();
+    this.bsModalRef = this.modalService.show(ModalFailureComponent);
+    this.bsModalRef.content.title = failureModalComponent.title;
+    this.bsModalRef.content.bodyItems = err.errors;
   }
 
 }
