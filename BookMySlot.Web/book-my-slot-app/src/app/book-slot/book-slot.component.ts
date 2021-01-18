61d8c1b141abdd098657d48416ef2535151d034c
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-18 12:05:56 +0400

    Book -slot

--- BookMySlot.Web/book-my-slot-app/src/app/book-slot/book-slot.component.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/book-slot/book-slot.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SlotSchedulerService } from '../services/slot-scheduler.service';
 import { BookSlots } from '../shared/book-slots';
+import { HttpStatusConstants } from '../shared/constants/http-status-constants';
 import { ResolverError } from '../shared/resolver-error';
 import { SlotScheduler } from '../shared/slot-scheduler';
 
@@ -12,15 +13,20 @@ import { SlotScheduler } from '../shared/slot-scheduler';
 })
 export class BookSlotComponent implements OnInit {
 
-  customerAvailableSlots: BookSlots;
+  customerAvailableSlots: BookSlots = new BookSlots();
+  resolverError: ResolverError;
 
   constructor(private slotSchedulerService: SlotSchedulerService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
+    this.customerAvailableSlots.slotModelsInforamtion = [];
     let initCustomerAvailableSlots: BookSlots | ResolverError = this.route.snapshot.data['resolvedBookCustomerSlots'];
 
     if (initCustomerAvailableSlots instanceof ResolverError) {
+      this.resolverError = initCustomerAvailableSlots;
+      if (this.resolverError.statusCode == HttpStatusConstants.NotFound) {
+        this.resolverError.errors = [];
+      }
     }
     else {
 
@@ -41,6 +47,7 @@ export class BookSlotComponent implements OnInit {
       .subscribe(
         (data: boolean) => {
           this.customerAvailableSlots.slotModelsInforamtion.splice(index, 1);
+
           console.log("get customer slots " + data);
           
           console.log(data);
