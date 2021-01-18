552c0596d3851149d4096b3d0b0b0f8380064489
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-18 13:20:05 +0400

    Added Request Id Guid

--- BookMySlot.Web/book-my-slot-app/src/app/shared/Interceptors/add-header.interceptor.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/shared/Interceptors/add-header.interceptor.ts
@@ -8,6 +8,8 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Guid } from "guid-typescript";
+
 
 @Injectable()
 export class AddHeaderInterceptor implements HttpInterceptor {
@@ -16,8 +18,9 @@ export class AddHeaderInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
+    let requestId = Guid.create().toString();
     let jsonReq: HttpRequest<any> = req.clone({
-      setHeaders: { 'Content-Type': 'application/json' }
+      setHeaders: { 'Content-Type': 'application/json', 'ui-request-id': requestId }
     });
 
     return next.handle(jsonReq)
