c32186896ff9ee003e777d890a4907062ecec10b
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-03-13 00:34:34 -0800

    TimeZone Updating

--- BookMySlot.Web/book-my-slot-app/src/app/customer-settings/customer-settings.component.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/customer-settings/customer-settings.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { CustomerSettingsService } from '../services/customer-settings.service';
 import { HttpStatusConstants } from '../shared/constants/http-status-constants';
 import { PageTitleConstants } from '../shared/constants/page-title-constants';
 import { CustomerSettings } from '../shared/customer-settings';
 import { ResolverError } from '../shared/resolver-error';
+import { ModalComponent } from '../shared/ui-controls/modal-component';
+import { ModalFailureComponent } from '../ui-controls/modal-failure/modal-failure.component';
+import { ModalSuccessComponent } from '../ui-controls/modal-success/modal-success.component';
 
 @Component({
   selector: 'app-customer-settings',
@@ -17,8 +21,10 @@ export class CustomerSettingsComponent implements OnInit {
 
   constructor(private customerSettingsService: CustomerSettingsService, private route: ActivatedRoute, private modalService: BsModalService, private title: Title) { }
 
-  public customerSettings: CustomerSettings;
+  public customerSettings = new  CustomerSettings();
   resolverError: ResolverError = new ResolverError();
+  private bsModalRef: BsModalRef;
+  private modalComponent = new ModalComponent();
 
   ngOnInit(): void {
     this.title.setTitle(PageTitleConstants.CustomerSettings);
@@ -35,7 +41,27 @@ export class CustomerSettingsComponent implements OnInit {
       this.customerSettings = initCustomerSettings;
       console.log("customer settings found " + this.customerSettings);
     }
-    
   }
+
+
+  onUpdate() {
+    this.customerSettings.timeZone = "Asia/Kolkata";
+    this.customerSettingsService.updateCustomerSettings(this.customerSettings)
+      .subscribe(
+        (data: boolean) => {
+          let successModalComponent = this.modalComponent.getSuccessModalComponent();
+          this.bsModalRef = this.modalService.show(ModalSuccessComponent);
+          this.bsModalRef.content.title = successModalComponent.title;
+          this.bsModalRef.content.bodyItems = successModalComponent.bodyItems;
+        },
+        (err: any) => {
+          let failureModalComponent = this.modalComponent.getFailureModalComponent();
+          this.bsModalRef = this.modalService.show(ModalFailureComponent);
+          this.bsModalRef.content.title = failureModalComponent.title;
+          this.bsModalRef.content.bodyItems = err.errors;
+        }
+      );
+  }
+
 
 }
