9c41bdecd012d3b53173d7e23b1b74aefaa39cc3
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-03-14 03:59:58 -0700

    DateTime Js Changes

--- BookMySlot.Web/book-my-slot-app/src/app/services/date-time-helper.service.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/services/date-time-helper.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DelimiterConstants } from '../shared/constants/delimiter-constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ export class DateTimeHelperService {
   }
 
   getTimeSpan(date: Date): string {
-    let timeSpan = date.getHours() + ":" + date.getMinutes() + ":" + "0";
+    //let timeSpan = date.getHours() + ":" + date.getMinutes() + ":" + "0";
+    let timeSpan = date.getHours() + DelimiterConstants.Time + date.getMinutes() + DelimiterConstants.Time + date.getSeconds();
     return timeSpan;
   }
 
 
   getDateString(date: Date): string {
-    let dateString = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+    let dateString = (date.getMonth() + 1) + DelimiterConstants.Date + date.getDate() + DelimiterConstants.Date + date.getFullYear();
     return dateString;
   }
 }
---
+++ BookMySlot.Web/book-my-slot-app/src/app/shared/constants/delimiter-constants.ts
@@ -0,0 +1,4 @@
+export const DelimiterConstants = {
+  Date: '-',
+  Time: ':',
+};
