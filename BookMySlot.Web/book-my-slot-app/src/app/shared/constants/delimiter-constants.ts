8f6b2efbaf6800a585b49b649b245a80f28deac9
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-03-21 05:24:25 -0700

    Noda Time Filter

--- BookMySlot.Web/book-my-slot-app/src/app/shared/constants/delimiter-constants.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/shared/constants/delimiter-constants.ts
@@ -2,4 +2,5 @@ export const DelimiterConstants = {
   Date: '-',
   Time: ':',
   CountryWithTimeZone: '::',
+  ZonedDateTime: ' ',
 };
--- BookMySlot.Web/book-my-slot-app/src/app/shared/pipes/noda-time.pipe.ts
+++ BookMySlot.Web/book-my-slot-app/src/app/shared/pipes/noda-time.pipe.ts
@@ -1,4 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { parse } from 'url';
+import { DelimiterConstants } from '../constants/delimiter-constants';
 
 @Pipe({
   name: 'nodaTime'
@@ -6,7 +8,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NodaTimePipe implements PipeTransform {
 
   transform(value: string, country: string): string {
-    return value + ' ' + country;
+
+    var arr = value.split(DelimiterConstants.ZonedDateTime);
+    let day = arr[0];
+    let time = arr[1];
+    let offset = arr[2];
+    let timeZone = arr[3];
+
+    let dayArray = day.split(DelimiterConstants.Date);
+    let month = parseInt(dayArray[0]);
+    let date = parseInt(dayArray[1]);
+    let year = parseInt(dayArray[2]);
+    let formattedDate = date + ',' + month + ' ' + year;
+
+    let formattedNodaTime = formattedDate + ' ' + time + ' ' + offset + ' ' + timeZone + ' (' + country + ') GMT';
+    return formattedNodaTime;
   }
 
 }
